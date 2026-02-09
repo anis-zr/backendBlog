318851ad779f76ea9ec6ee0c9ae49ab7d8890d4b
unknown <aniszerouali40@gmail.com>
2026-02-09 01:55:09 +0100

    ne

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 
 app.use(cors({
-    origin:"http://agritech-ten-eta.vercel.app",
+    origin:"https://agritech-ten-eta.vercel.app",
     methods: ["GET","POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials:true,
